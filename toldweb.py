664c5b57fae8cea8b19102b753d692ba56e3f034
Jack Curran <jack7curran@gmail.com>
2023-08-16 16:33:25 -0400

    better readability

--- toldweb.py
+++ toldweb.py
@@ -64,29 +64,41 @@ class ToldCard:
         takeoff_moment = fuel_data["zerofuel"]["moment"] + fuel_moment_count
 
         # Assigning calculated values to class attributes
-        self.basic_empty = {"weight": self.empty_weight, "arm": self.empty_moment / self.empty_weight, 
+        self.basic_empty = {"weight": self.empty_weight, 
+                            "arm": self.empty_moment / self.empty_weight, 
                             "moment": self.empty_moment}
-        self.pilot_and_passenger = {"weight": data["pilot"]["weight"], "arm": data["pilot"]["arm"],
+        self.pilot_and_passenger = {"weight": data["pilot"]["weight"], 
+                                    "arm": data["pilot"]["arm"],
                                     "moment": data["pilot"]["weight"] * data["pilot"]["arm"]}
-        self.rear_passengers = {"weight": data["backseat"]["weight"], "arm": data["backseat"]["arm"],
-                                "moment": data["backseat"]["weight"] * data["backseat"]["arm"]}
-        self.baggage_area_1 = {"weight": data["baggage1"]["weight"], "arm": data["baggage1"]["arm"],
+        self.rear_passengers = {"weight": data["backseat"]["weight"], 
+                                "arm": data["backseat"]["arm"],
+                                "moment": data["backseat"]["weight"] 
+                                * data["backseat"]["arm"]}
+        self.baggage_area_1 = {"weight": data["baggage1"]["weight"], 
+                               "arm": data["baggage1"]["arm"],
                                "moment": data["baggage1"]["weight"] * data["baggage1"]["arm"]}
-        self.baggage_area_2 = {"weight": data["baggage2"]["weight"], "arm": data["baggage2"]["arm"],
+        self.baggage_area_2 = {"weight": data["baggage2"]["weight"], 
+                               "arm": data["baggage2"]["arm"],
                                "moment": data["baggage2"]["weight"] * data["baggage2"]["arm"]}
         self.zero_fuel_weight = {"weight": weight_count, "arm": moment_count / weight_count,
                                  "moment": moment_count}
-        self.fuel = {"weight": fuel_data["fuel"]["weight"], "arm": fuel_data["fuel"]["arm"],
+        self.fuel = {"weight": fuel_data["fuel"]["weight"], 
+                     "arm": fuel_data["fuel"]["arm"],
                      "moment": fuel_data["fuel"]["weight"] * fuel_data["fuel"]["arm"]}
-        self.ramp_weight = {"weight": takeoff_weight, "arm": takeoff_arm, 
+        self.ramp_weight = {"weight": takeoff_weight, 
+                            "arm": takeoff_arm, 
                             "moment": takeoff_moment}
-        self.start_taxi_runup = {"weight": "-7", "arm": "48", 
+        self.start_taxi_runup = {"weight": "-7", 
+                                 "arm": "48", 
                                  "moment": "-336"}
-        self.takeoff_weight = {"weight": takeoff_weight - 7, "arm": (takeoff_moment - 336) / (takeoff_weight - 7), 
+        self.takeoff_weight = {"weight": takeoff_weight - 7, 
+                               "arm": (takeoff_moment - 336) / (takeoff_weight - 7), 
                                "moment": takeoff_moment - 336}
-        self.fuel_burn = {"weight": "-72", "arm": "48", 
+        self.fuel_burn = {"weight": "-72", 
+                          "arm": "48", 
                           "moment": "-3456"}
-        self.landing_weight = {"weight": self.takeoff_weight["weight"] - 72, "arm": (self.takeoff_weight["moment"] - 3456) / (self.takeoff_weight["weight"] - 72),
+        self.landing_weight = {"weight": self.takeoff_weight["weight"] - 72, 
+                               "arm": (self.takeoff_weight["moment"] - 3456) / (self.takeoff_weight["weight"] - 72),
                                "moment": self.takeoff_weight["moment"] - 3456}
 
     def sort(self):
